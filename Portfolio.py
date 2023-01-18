79980079faefcabe097eb666e285545a034a0625
natew <nateweil1993@gmail.com>
2023-01-17 22:43:43 -0500

    Started UI

--- Portfolio.py
+++ Portfolio.py
@@ -1,1 +1,20 @@
 import streamlit as stream
+
+stream.set_page_config(layout="wide")
+
+stream.title("Home")
+stream.title("")
+
+col_one, col_two = stream.columns(2)
+
+with col_one:
+    stream.image("images/me.png", width=300)
+
+with col_two:
+    stream.subheader("Nate Weil")
+
+    bio = """
+    This is my bio. Yay!
+    
+    """
+    stream.info(bio)
